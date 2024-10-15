b144d388c9e2f9312a93dca3e8278df5a2a46ed2
David <128605626+Davidbld@users.noreply.github.com>
2024-10-15 12:03:22 +0000

    Up TD3

--- TD3/Ex2.cpp
+++ TD3/Ex2.cpp
@@ -5,8 +5,8 @@
 
 
 int main(int argc, char *argv[]){
-    double x[4]={0.0,1.0,1.0,0.0};
-    double y[4]={0.0,0.0,1.0,1.0};
+    double x[4]={0.0000000003,1.0000000003,1.0000000003,0.0000000003};
+    double y[4]={0.0000000003,0.0000000003,1.0000000003,1.0000000003};
 
     std::ofstream write_output("x_et_y.dat");
     assert(write_output.is_open());
@@ -16,6 +16,11 @@ int main(int argc, char *argv[]){
 
     for (int i = 0; i < 4; i++)
     {
+        if (x[i]>0)
+        {
+            write_output<<"+";
+        }
+        
         write_output.precision(10);
         write_output<<x[i]<<" ";
         write_output.flush();
@@ -26,6 +31,11 @@ int main(int argc, char *argv[]){
     
     for (int i = 0; i < 4; i++)
     {
+        if (y[i]>0)
+        {
+            write_output<<"+";
+        }
+
         write_output.precision(10);
         write_output<<y[i]<<" ";
         write_output.flush();
---
+++ TD3/Ex3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <cmath> //fonction cubique
+
+int main(int argc, char *argv[]){
+
+    double matriceClassique[2][2]={{4,10},{1,1}};
+    double matriceInversee[2][2]={{0,0},{0,0}};
+
+    //calcul du déterminant
+    double detA=(matriceClassique[0][0]*matriceClassique[1][1])-(matriceClassique[0][1]*matriceClassique[1][0]);
+    std::cout<<"Déterminant : "<<detA<<"\n";
+    if (detA!=0)
+    {
+        std::cout<<"La matrice est inversible"<<"\n";
+        matriceInversee[0][0]=matriceClassique[1][1];
+        matriceInversee[0][1]=-matriceClassique[1][0];
+        matriceInversee[1][0]=-matriceClassique[0][1];
+        matriceInversee[1][1]=matriceClassique[0][0];
+    }
+    else {
+        std::cout<<"Erreur"<<"\n";
+        assert(detA=0);
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            std::cout<<1/(detA)*matriceInversee[i][j]<<"\t";
+        }
+        std::cout<<"\n";
+    }
+    
+    return 0;
+}
